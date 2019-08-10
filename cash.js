68b91313e155f54e76d2b3d29971b0bf7a6f08d1
Arif Dogan <arifdogan@8black.co>
2019-08-10 05:23:01 +0300

    Husky Added

--- cash.js
+++ cash.js
@@ -1,9 +1,4 @@
-var C = {};
-C.FirstTestCase = function () {
-    'use strict';
-    return [50, 20, 20];
-};
-C.SecondTestCase = () => {
-  return "Arif"
-}
-module.exports = C;         
+const C = {};
+C.FirstTestCase = () => [50, 20, 20];
+C.SecondTestCase = () => 'Arif';
+module.exports = C;
--- test.js
+++ test.js
@@ -1,18 +1,19 @@
-var assert = require("assert");
-var C = require('./cash.js');
+/* eslint-disable no-undef */
+const assert = require('assert');
+const C = require('./cash.js');
 
-describe('Basic C Tests', function(){
-  describe('Module C', function(){
-    it('Type Of C', function(){
+describe('Basic C Tests', () => {
+  describe('Module C', () => {
+    it('Type Of C', () => {
       assert.equal(typeof C, 'object');
-    })
+    });
 
     it('First Test Case', () => {
-      assert.deepEqual(C.FirstTestCase(),[50,20,20])
-    })
+      assert.deepEqual(C.FirstTestCase(), [50, 20, 20]);
+    });
 
     it('Second Test Case', () => {
-      assert.deepEqual(C.SecondTestCase(),"Arif")
-    })
-  })
+      assert.deepEqual(C.SecondTestCase(), 'Arif');
+    });
+  });
 });
